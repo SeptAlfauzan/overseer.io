57ab559ec729f8e3858b2e04d04d491eb9a7b258
SeptAlfauzan <alfauzansepta@gmail.com>
2024-03-21 18:30:42 +0700

    add: card widget

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,14 +1,21 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import core.widgets.CardInfo
 
 @Composable
 @Preview
@@ -16,10 +23,32 @@ fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Surface(color = Color(0xFFf6f6f6)) {
+            Box(modifier = Modifier.padding(24.dp)) {
+
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Text(
+                        "Selamat datang",
+                        style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Bold),
+                        modifier = Modifier.padding(bottom = 24.dp)
+                    )
+                    Text(
+                        "Hasil report hari ini",
+                        style = MaterialTheme.typography.body1.copy(color = Color.Black.copy(alpha = 0.4f)),
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                    Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                        CardInfo(label = "Study", value = 10, icon = Icons.Default.Info)
+                        CardInfo(label = "On-Phone", value = 10, icon = Icons.Default.Phone)
+                        CardInfo(label = "Sleep", value = 10, icon = Icons.Default.Info)
+                    }
+                    Button(onClick = {
+                        text = "Hello, Desktop!"
+                    }) {
+                        Text(text)
+                    }
+                }
+            }
         }
     }
 }
---
+++ src/jvmMain/kotlin/core/widgets/CardInfo.kt
@@ -0,0 +1,44 @@
+package core.widgets
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardInfo(label: String, value: Int, icon: ImageVector) {
+    Card {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.width(224.dp)) {
+                Text(
+                    label,
+                    style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight(700)),
+                    modifier = Modifier.padding(bottom = 24.dp)
+                )
+                Icon(
+                    icon,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(32.dp)
+                        .border(
+                            border = BorderStroke(2.dp, color = Color.Black),
+                            shape = CircleShape
+                        )
+                        .padding(8.dp)
+                )
+            }
+            Text(value.toString(), style = MaterialTheme.typography.h3, modifier = Modifier.align(Alignment.Bottom))
+        }
+    }
+}
